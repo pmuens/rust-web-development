1e12783bbb74234a19b252b2279655b405285985
Philipp Muens <philipp@muens.io>
2023-10-05 08:37:55 +0200

    Setup CORS

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use serde::Serialize;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
-use warp::{http::StatusCode, reject::Reject, Filter, Rejection, Reply};
+use warp::{
+    filters::cors::CorsForbidden, http::Method, http::StatusCode, reject::Reject, Filter,
+    Rejection, Reply,
+};
 
 #[derive(Debug, Serialize)]
 struct Question {
@@ -55,14 +58,19 @@ async fn get_questions() -> Result<impl Reply, Rejection> {
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(InvalidId) = r.find() {
+    if let Some(error) = r.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
-            "No valid ID presented",
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else {
         Ok(warp::reply::with_status(
-            "Route not found",
+            "Route not found".to_string(),
             StatusCode::NOT_FOUND,
         ))
     }
@@ -70,12 +78,17 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
 #[tokio::main]
 async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+
     let get_items = warp::get()
         .and(warp::path("questions").and(warp::path::end()))
         .and_then(get_questions)
         .recover(return_error);
 
-    let routes = get_items;
+    let routes = get_items.with(cors);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
