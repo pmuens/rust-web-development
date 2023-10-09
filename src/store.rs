ab8bb350480035a5a839cbd9c6d736b0f1110f5d
Philipp Muens <philipp@muens.io>
2023-10-09 19:35:31 +0200

    Implement sign-in functionality

--- src/store.rs
+++ src/store.rs
@@ -5,7 +5,7 @@ use sqlx::Row;
 use crate::types::answer::NewAnswer;
 use crate::types::question::NewQuestion;
 use crate::types::{
-    account::Account,
+    account::{Account, AccountId},
     answer::{Answer, AnswerId},
     question::{Question, QuestionId},
 };
@@ -154,6 +154,25 @@ impl Store {
                     db_message = e.as_database_error().unwrap().message(),
                     constraint = e.as_database_error().unwrap().constraint().unwrap()
                 );
+                Err(Error::DatabaseQueryError(e))
+            }
+        }
+    }
+
+    pub async fn get_account(self, email: String) -> Result<Account, Error> {
+        match sqlx::query("select * from accounts where email = $1;")
+            .bind(email)
+            .map(|row: PgRow| Account {
+                id: Some(AccountId(row.get("id"))),
+                email: row.get("email"),
+                password: row.get("password"),
+            })
+            .fetch_one(&self.connection)
+            .await
+        {
+            Ok(account) => Ok(account),
+            Err(e) => {
+                tracing::event!(tracing::Level::ERROR, "{:?}", e);
                 Err(Error::DatabaseQueryError(e))
             }
         }
