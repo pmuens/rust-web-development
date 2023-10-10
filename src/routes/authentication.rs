c482974bf4f4acce98d7130f3cf7ce400c96166c
Philipp Muens <philipp@muens.io>
2023-10-10 12:34:28 +0200

    Add tests for authorization

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -86,8 +86,26 @@ fn issue_token(account_id: AccountId) -> String {
     paseto::tokens::PasetoBuilder::new()
         .set_encryption_key(&Vec::from(key.as_bytes()))
         .set_expiration(&dt)
-        .set_not_before(&Utc::now())
         .set_claim("account_id", serde_json::json!(account_id))
         .build()
         .expect("Failed to construct paesto token with builder!")
+}
+
+#[cfg(test)]
+mod authentication_tests {
+    use super::{auth, env, issue_token, AccountId};
+
+    #[tokio::test]
+    async fn post_questions_auth() {
+        env::set_var("PASETO_KEY", "RANDOM WORDS WINTER MACINTOSH PC");
+        let token = issue_token(AccountId(3));
+
+        let filter = auth();
+
+        let res = warp::test::request()
+            .header("Authorization", token)
+            .filter(&filter);
+
+        assert_eq!(res.await.unwrap().account_id, AccountId(3));
+    }
 }
